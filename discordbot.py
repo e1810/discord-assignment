9c964503a92bdd944499930444bca6f6dd732e99
e1810 <v.iceele1810@gmail.com>
2020-05-30 19:02:28 +0900

    ユーザごとに通知を分別

--- discordbot.py
+++ discordbot.py
@@ -27,43 +27,60 @@ async def man(message):
         ret += '------------------------'
     await message.channel.send(ret)
 
+
 async def add(message):
     print('add assignment')
     msg = message.content.split(' ')
-    try:
+
+    if len(msg) == 4:
         title, deadline, memo = msg[1:]
-
-        conn.hset(title, 'title', title)
-        conn.hset(title, 'deadline', deadline)
-        conn.hset(title, 'memo',  memo)
-
+        key = title + message.author.mention
+        conn.hset(key, 'title', title)
+        conn.hset(key, 'deadline', deadline)
+        conn.hset(key, 'memo',  memo)
+        conn.hset(key, 'target', message.author.mention)
         await message.channel.send('課題を追加しました！')
-    except:
+    elif len(msg) == 5:
+        title, deadline, memo, target = msg[1:]
+        key = title + target
+        conn.hset(key, 'title', title)
+        conn.hset(key, 'deadline', deadline)
+        conn.hset(key, 'memo', memo)
+        conn.hset(key, 'target', target)
+        await message.channel.send('課題を追加しました！')
+    else:
         await message.channel.send('入力形式が間違っています。')
+
 
 async def delete(message):
     print('delete assignment')
     try:
         title = message.content.split(' ')[1]
-        if conn.exists(title):
-            conn.delete(title)
+        if conn.exists(title+message.author.mention):
+            conn.delete(title+message.author.mention)
+            await message.channel.send('課題を削除しました')
+        elif conn.exists(title+'@everyone'):
+            conn.delete(title+'@everyone')
             await message.channel.send('課題を削除しました')
         else:
             await message.channel.send('そのような課題はありません')
     except:
         await message.channel.send('入力形式が間違っています。')
 
+
 async def ls(message):
     print('sending list')
+    cnt = 0
     ret = '課題一覧\n'
-    for i, title in enumerate(conn.keys()):
-        ret += '------------------------\n'
-        ret += '{}. {}\n'.format(i + 1, title)
-        ret += '締切: {}\n'.format(conn.hget(title, 'deadline'))
-        ret += '備考: {}\n'.format(conn.hget(title, 'memo'))
-        ret += '------------------------\n'
-
-    ret += '現在、{}個の課題が出されています。'.format(len(conn.keys()))
+    for i, key in enumerate(conn.keys()):
+        if conn.hget(key, 'target') in (message.author.mention, '@everyone'):
+            ret += '------------------------\n'
+            ret += '{}. {}\n'.format(cnt + 1, conn.hget(key, 'title'))
+            ret += '締切: {}\n'.format(conn.hget(key, 'deadline'))
+            ret += '備考: {}\n'.format(conn.hget(key, 'memo'))
+            ret += '------------------------\n'
+            cnt += 1
+    ret += '現在、{}個の課題が出されています。'.format(cnt)
     await message.channel.send(ret)
 
 async def close(message):
@@ -79,7 +96,7 @@ COMMANDS = {
     },
     'add': {
         'description': '新しい課題を追加します。',
-        'use': '!add \{課題名\} \{締切\} \{備考\}',
+        'use': '!add \{課題名\} \{締切\} \{備考\} \{課題対象の生徒\}',
         'alias': '!a',
         'func': add
     },
@@ -100,13 +117,14 @@ COMMANDS = {
         'use': '!exit',
         'alias': '!ex',
         'func': close
-    }
+    },
 }
 
 @client.event
 async def on_ready():
     print('I\'m ready')
     await loop.start()
+
 
 @client.event
 async def on_message(message):
@@ -129,33 +147,34 @@ async def loop():
     remain_1day = []
     remain_3day = []
 
-    for title in conn.keys():
-        deadline_str = str(tommorow.year) + '/' + conn.hget(title, 'deadline')
+    for key in conn.keys():
+        deadline_str = str(tommorow.year) + '/' + conn.hget(key, 'deadline')
         deadline = datetime.datetime.strptime(deadline_str, '%Y/%m/%d')
         if deadline < today:
-            conn.hdel(title)
+            conn.delete(key)
         elif deadline <= tommorow:
-            remain_1day.append(title)
+            remain_1day.append(key)
         elif deadline <= three_days_later:
-            remain_3day.append(title)
+            remain_3day.append(key)
 
     if len(remain_1day)+len(remain_3day)==0:
         return
 
     ret = '{}個の課題の提出期限が迫っています！\n'.format(len(remain_1day)+len(remain_3day))
-    for i, title in enumerate(remain_3day):
+    for i, key in enumerate(remain_3day):
         ret += '------------------------\n'
-        ret += '{}. {}\n'.format(i + 1, title)
-        ret += '締切: {}\n'.format(conn.hget(title, 'deadline'))
-        ret += '備考: {}\n'.format(conn.hget(title, 'memo'))
+        ret += '{}. {}\n'.format(i + 1, conn.hget(key, 'title'))
+        ret += '締切: {}\n'.format(conn.hget(key, 'deadline'))
+        ret += '備考: {}\n'.format(conn.hget(key, 'memo'))
         ret += '------------------------\n'
+
     if remain_1day:
         ret += '↓↓↓あと1日もないよ↓↓↓\n'
-    for i, title in enumerate(remain_1day):
+    for i, key in enumerate(remain_1day):
         ret += '------------------------\n'
-        ret += '{}. {}\n'.format(i + 1, title)
-        ret += '締切: {}\n'.format(conn.hget(title, 'deadline'))
-        ret += '備考: {}\n'.format(conn.hget(title, 'memo'))
+        ret += '{}. {}\n'.format(i + 1, (key))
+        ret += '締切: {}\n'.format(conn.hget(key, 'deadline'))
+        ret += '備考: {}\n'.format(conn.hget(key, 'memo'))
         ret += '------------------------\n'
         
 
